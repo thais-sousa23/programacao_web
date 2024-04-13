448e45fcab37a39db75c81e9c3abe6d6ee6a8819
Thais Sousa <151460987+thais-sousz@users.noreply.github.com>
2024-04-12 22:19:33 -0300

    Add files via upload

---
+++ atividades - 2 unidade da disciplina/primeira aula/BD.php
@@ -0,0 +1,8 @@
+<?php
+
+    $bd = mysqli_connect("127.0.0.1", "root", "", "hospital");
+    $sql = "insert into paciente() value ()";
+    mysqli_query($bd, $sql);
+    mysqli_close($bd);
+
+?>
---
+++ atividades - 2 unidade da disciplina/primeira aula/Consulta_hospital.sql
@@ -0,0 +1,4 @@
+INSERT INTO paciente 
+(id, nome, data_nascimento, genero, data_admissao, esta_ativo)
+VALUES
+(1,'Thais Sousa', '2004-09-23','feminino','2024-04-11 21:37:00', true)
